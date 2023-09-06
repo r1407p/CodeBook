4568f4d81705c112ce0bcbe93deb0414786fa862
tony <tonychi123@gmail.com>
2023-09-06 10:22:01 +0800

    style: rename spanning tree

--- Tree/Spanning Tree.cpp
+++ Tree/Spanning Tree.cpp
@@ -1,6 +1,3 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 const int V = 100, E = 1000;
 struct Edge {int a, b, c;} e[E];	// edge list
 bool operator<(Edge e1, Edge e2) {return e1.c < e2.c;}
@@ -10,8 +7,7 @@ void init() {for (int i=0; i<V; ++i) p[i] = i;}
 int find(int x) {return x == p[x] ? x : (p[x] = find(p[x]));}
 void merge(int x, int y) {p[find(x)] = find(y);}
 
-void Kruskal()
-{
+void Kruskal(){
 	init();
 	sort(e, e+E);
 	int i, j;
